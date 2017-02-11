475dab40da97489b1c5afd01c49fb718b1b4731f
kghite <kghite@gmail.com>
2017-02-11 16:39:54 -0500

    README instructions for opencv trainer

---
+++ opencv_training/python_scripts/panel_detector.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+# Button panel cascade file
+panel_cascade = cv2.CascadeClassifier('button_panel_classifier.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    panels = panel_cascade.detectMultiScale(gray, 50, 50)
+    
+    # Mark the panel with a rectangle
+    for (x,y,w,h) in watches:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
