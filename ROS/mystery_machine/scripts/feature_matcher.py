47058d96688c0253cf8659755a7b134e50c5ad57
laurengulland <lauren.gulland@students.olin.edu>
2017-04-22 16:02:16 -0400

    working feature matcher, just need bag implementation now. also has testing features for printing images to debug with.

--- ROS/mystery_machine/scripts/feature_matcher.py
+++ ROS/mystery_machine/scripts/feature_matcher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Feature matcher. Subscribbes to map, finds feature location in thresholded occupancy grid"""
+"""Feature matcher. Subscribes to map, finds feature location in thresholded occupancy grid"""
 """Publishes marker to that location"""
 import rospy
 from nav_msgs.msg import OccupancyGrid
@@ -8,6 +8,7 @@ import numpy as np
 import cPickle as pickle
 import thread
 from scipy import misc, special,stats
+from matplotlib import pyplot as plt
 
 
 
@@ -24,32 +25,45 @@ class Feature_Matcher():
         #path to feature to match in greyscale image form
         self.featurepath = "elevator_template.jpg"
         self.testpath = "test_image.jpg" #for test
-        #load feature to compare to 
+        #load feature to compare to
         #self.features = np.load(self.featurepath)
         #image_cv2 = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
-        
+
         # load template sign images as grayscale
         self.elevators = cv2.imread(self.featurepath,0)
         #load map template as graysacle(for testing)
-        self.mapimage = cv2.imread(self.testpath,0)
-        
+        self.map_image = cv2.imread(self.testpath,0)
+
         map_pub = rospy.Publisher('/elevatormap',OccupancyGrid, queue_size = 10)
- 
+
+        self.test=False #whether or not to run the test picture printout
+
     def match_features(self):
         """ Matches features against template and publishes map with elevator location"""
         #load images into memory so if they're updated by a callback nothing gets broken
-        
+
         #map_image = self.last_map
 
-        map_image = self.mapimage
+        map_image = self.map_image
         elevator_image = self.elevators
-        #
-        res =cv2.matchTemplate(map_image,elevator_image,'cv2.TM_CCOEFF')
+        res = cv2.matchTemplate(map_image,elevator_image,eval('cv2.TM_CCOEFF'))
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         top_left = max_loc
         confidence = max_val
+
+        if self.test==True: #print picture things for debugging!
+            w,h = elevator_image.shape[::-1]
+            test_img = map_image.copy()
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            cv2.rectangle(test_img,top_left, bottom_right, 255, 2)
+            plt.subplot(121),plt.imshow(res,cmap = 'gray')
+            plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+            plt.subplot(122),plt.imshow(test_img,cmap = 'gray')
+            plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+            plt.suptitle('Testing!\n To zoom in, use Zoom To Rectangle Tool')
+            plt.show()
         #create new blank array
-        newgridarr = np.zeroes(self.map_width*self.map_height)
+        newgridarr = np.zeros(self.map_width*self.map_height)
         #make corresponding value 100% chance of occupancy
         newgridarr[max_loc[0]*top_left[1]] = 100
         #make occupancy grid.
@@ -66,7 +80,7 @@ class Feature_Matcher():
 
     def process_occupancy_grid(self, occupancy_grid):
         '''
-        Input: Occuapancy grid 
+        Input: Occuapancy grid
         Output: Binary openCV image of occupancy grid
         '''
         #read into numpy array and shape correctly
@@ -81,9 +95,10 @@ class Feature_Matcher():
 
     ##Main
 
-    def run(self):    
+    def run(self):
         self.sleepy.sleep()
         self.match_features()
 
 feature = Feature_Matcher()
+feature.test=False #set me to true to get printing images for debugging!
 feature.run()
